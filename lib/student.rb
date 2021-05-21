465dbfcd87a18ca974abafb83fa702ffe0e9c7d3
Kayla Luebbert <luebbertkayla@gmail.com>
2021-05-21 14:54:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,64 +1,59 @@
 require_relative "../config/environment.rb"
 
 class Student
-
   attr_accessor :id, :name, :grade
-  
-  def initialize(id = nil, name, grade)
-    @id = nil 
-    @name = name 
-    @grade = grade
-  end 
-  
-  def self.create_table 
-    sql = <<-SQL
+
+  def initialize(id=nil, name, grade)
+    @id, @name, @grade = id, name, grade
+  end
+
+  def self.create_table
+    sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT, 
-        grade INTEGER
-        )
-      SQL
+        grade TEXT
+      )
+    SQL
     DB[:conn].execute(sql)
-  end 
+  end
 
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end 
-  
-  def save 
-    if self.id 
+  def self.drop_table 
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  end
+
+  def save
+    if self.id
       self.update
-    else 
-      sql = <<-SQL
-        INSERT INTO students (name, grade) 
-        VALUES (?, ?)
+    else
+      sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
       SQL
-      
+
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end 
-  end 
-      
+    end
+  end
+
   def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
-  
+
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
     grade = row[2]
     self.new(id, name, grade)
-  end
+  end 
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
-  end 
-  
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
+
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
