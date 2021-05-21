fb397f18a23cbdfdb653b95779b4d56268fc5793
Kayla Luebbert <luebbertkayla@gmail.com>
2021-05-21 14:34:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,5 +61,9 @@ class Student
        self.new_from_db(row)
      end
   end 
-
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
