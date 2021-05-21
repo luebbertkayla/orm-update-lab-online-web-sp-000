1ad39cf62e8dc2a499802842ca0786fdf4128563
Kayla Luebbert <luebbertkayla@gmail.com>
2021-05-21 14:44:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,8 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(name)
+    result = DB[:conn].execute(sql, name)[0]
+    
     
     
      end
