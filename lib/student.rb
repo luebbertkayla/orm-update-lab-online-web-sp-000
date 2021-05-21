7d462ecbf00059dcb205fd1b654a653bdea8d8c7
Kayla Luebbert <luebbertkayla@gmail.com>
2021-05-21 13:46:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].excute("SELECT last_insert_rowid() FROM students")
+    @id = DB[:conn].excute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
       
 
